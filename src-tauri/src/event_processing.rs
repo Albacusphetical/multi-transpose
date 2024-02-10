edde543576b2ff73af01c5542178b8066eb85af9
Albacusphetical <abcgames007@gmail.com>
2024-02-10 00:42:53 +0000

    Switched from inputbot to rdev (better cross-platform support), made a patch for rdev to make it compatible with directinput (at least for windows)

--- src-tauri/src/event_processing.rs
+++ src-tauri/src/event_processing.rs
@@ -1,11 +1,9 @@
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
-use inputbot::BlockInput::{Block, DontBlock};
-use inputbot::KeybdKey;
 use tauri::{AppHandle, Event, Manager};
 use serde_json::{json, Value};
 use serde_json::Value::Object;
-use crate::keyboard::{PAUSE_BIND, TRANSPOSE_DOWN_BIND, transpose_down_bind_fn, TRANSPOSE_UP_BIND, transpose_up_bind_fn, PREVIOUS_TRANSPOSE_BIND, NEXT_TRANSPOSE_BIND};
+use crate::keyboard::{PAUSE_BIND, TRANSPOSE_DOWN_BIND, previous_transpose_bind_fn, TRANSPOSE_UP_BIND, next_transpose_bind_fn, PREVIOUS_TRANSPOSE_BIND, NEXT_TRANSPOSE_BIND};
 use crate::{PAUSED, SELECTED_INDEX, TRANSPOSES, CURRENT_TRANSPOSE};
 
 #[derive(Clone, serde::Serialize)]
@@ -69,61 +67,11 @@ unsafe fn set_keybind_event(keybind: &Value, app_handle: AppHandle, last_press: 
     let bind_name = keybind.get("name").unwrap().as_str().unwrap();
 
     match bind_name {
-        "pause" => {
-            if PAUSE_BIND != None {
-                KeybdKey::from(PAUSE_BIND.unwrap()).unbind();
-            }
-
-            PAUSE_BIND = Some(keycode);
-
-            KeybdKey::from(PAUSE_BIND.unwrap()).bind(move || {
-                if TRANSPOSE_UP_BIND.is_none()
-                    || TRANSPOSE_DOWN_BIND.is_none()
-                    || NEXT_TRANSPOSE_BIND.is_none()
-                    || PREVIOUS_TRANSPOSE_BIND.is_none()
-                {
-                    // prevent resuming if there are required keybindings still
-                    return;
-                }
-
-                PAUSED = !PAUSED;
-
-                let json = serde_json::to_string(&json!({"paused": PAUSED})).unwrap();
-                app_handle.lock().unwrap().emit_all("frontend_event", Payload { message: json });
-            })
-        },
-        "transpose_up" => {
-            TRANSPOSE_UP_BIND = Some(keycode);
-        },
-        "transpose_down" => {
-            TRANSPOSE_DOWN_BIND = Some(keycode);
-        },
-        "next_transpose" => {
-            if NEXT_TRANSPOSE_BIND != None {
-                KeybdKey::from(NEXT_TRANSPOSE_BIND.unwrap()).unbind();
-            }
-
-            NEXT_TRANSPOSE_BIND = Some(keycode);
-
-            KeybdKey::from(NEXT_TRANSPOSE_BIND.unwrap()).bind(move || {
-                let app_handle = app_handle.lock().unwrap().clone();
-                let last_press = Arc::clone(&last_press);
-                transpose_up_bind_fn(app_handle, last_press);
-            });
-        },
-        "previous_transpose" => {
-            if PREVIOUS_TRANSPOSE_BIND != None {
-                KeybdKey::from(PREVIOUS_TRANSPOSE_BIND.unwrap()).unbind();
-            }
-
-            PREVIOUS_TRANSPOSE_BIND = Some(keycode);
-
-            KeybdKey::from(PREVIOUS_TRANSPOSE_BIND.unwrap()).bind(move || {
-                let app_handle = app_handle.lock().unwrap().clone();
-                let last_press = Arc::clone(&last_press);
-                transpose_down_bind_fn(app_handle, last_press)
-            })
-        },
+        "pause" => PAUSE_BIND = Some(keycode),
+        "transpose_up" => TRANSPOSE_UP_BIND = Some(keycode),
+        "transpose_down" => TRANSPOSE_DOWN_BIND = Some(keycode),
+        "next_transpose" => NEXT_TRANSPOSE_BIND = Some(keycode),
+        "previous_transpose" => PREVIOUS_TRANSPOSE_BIND = Some(keycode),
         _ => {}
     }
 }
